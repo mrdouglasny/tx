3827974a97e515917beef6bff2363fde68bcb490
William Hu <williamhu@college.harvard.edu>
2025-06-17 13:20:44 -0400

    Simple and degree invariants

--- Tx/Edge.lean
+++ Tx/Edge.lean
@@ -1,7 +1,11 @@
 import Mathlib.Combinatorics.SimpleGraph.Coloring
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Sym.Sym2
 import Mathlib.Data.ENat.Basic
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Set.Card
 
 /-!
 Lean 4 counterpart of a subset of the invariants provided by the
@@ -13,6 +17,16 @@ Currently covered
 * `chromatic_number`  – smallest number of colours in a proper vertex colouring;
 * `clique_number`     – size of a largest clique (maximum clique size);
 * `edge_cover_number` – size *or* `⊤` of a smallest edge cover.
+* `independence_number` - size of largest independent set
+* `matching number` - NOT DONE YET
+* `vertex cover number` - size of minimum vertex cover
+* `annihilation number` - NOT DONE YET
+* `average degree` - average of vertex degrees
+* `degree` - degree of a given vertex
+* `degree sequence` - list all vertex degrees, preserving duplicates
+* `harmonic index` - sum of 2 / (deg(u) + deg(v)) over all edges {u, v}
+* `maximum degree` - max degree, duh
+* `minimum degree` - min degree, duh
 
 > ### About `edge_cover_number`
 > Mathlib&nbsp;4 does not yet package an "edge cover number" out of the box, so
@@ -30,7 +44,8 @@ open scoped Classical
 
 universe u
 
-variable {V : Type u}
+-- For now, we will only work over finite graphs.
+variable {V : Type u} [Fintype V]
 
 /-- The **chromatic number** of a graph `G`.
 
@@ -52,6 +67,36 @@ that `clique_number` lines up with *GraphCalc*’s
 -/
 noncomputable def clique_number (G : SimpleGraph V) : ℕ :=
   G.cliqueNum
+
+noncomputable def independence_number (G : SimpleGraph V) : ℕ :=
+  G.indepNum
+
+noncomputable def degree (G : SimpleGraph V) (v : V) :=
+  G.degree v
+
+/--
+average_degree
+
+Obviously this is always a rational number. I've made it a real for now,
+but we can change it back to a rational in ℚ if needed.
+-/
+noncomputable def average_degree (G : SimpleGraph V) : ℝ :=
+  let total_degree := ∑ v, (G.degree v)
+  total_degree / Fintype.card V
+
+/--
+degree_sequence: 'the list of vertex degrees in the graph,
+optionally sorted in nonincreasing order.'
+
+Because two vertices can have the same degree, we cannot use a Finset here -
+Finsets do not allow duplicates. We can either use MultiSet (allows duplicates,
+but no order) or List (duplicates and order).
+
+I haven't implemented the 'optional sorting' part yet; how important is that?
+-/
+noncomputable def degree_sequence (G : SimpleGraph V) : List ℕ :=
+-- (Finset.univ : Finset V).val.map (λ v => G.degree v) will return a MultiSet instead
+   (Finset.univ : Finset V).toList.map (λ v => G.degree v)
 
 /-- A predicate saying that a set of edges `C` is an **edge cover** of `G`:
 * every edge in `C` is indeed an edge of `G`, *and*
