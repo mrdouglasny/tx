3bd6205611d008904bc3df75bf2b656f5c15f5ea
William Hu <williamhu@college.harvard.edu>
2025-07-16 21:08:33 -0400

    Rainbow Domination

--- Tx/Edge.lean
+++ Tx/Edge.lean
@@ -3,6 +3,7 @@ import Mathlib.Data.Sym.Sym2
 import Mathlib.Data.ENat.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Set.Card
+import Mathlib.Data.Set.Basic
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Real.Archimedean -- needed for sInf and sSup (set infimum/supremum) on the reals
 import Mathlib.Combinatorics.SimpleGraph.Coloring
@@ -265,6 +266,28 @@ noncomputable def well_splitting_number (G : SimpleGraph V) :=
       ∧ n = C.ncard}
   sInf S
 
+-- RAINBOW DOMINATION
+
+-- For a graph G, a set C is a rainbow dominating set for the coloring f:
+-- A rainbow dominating set is a set of nodes
+-- such that every uncolored node is adjacent to nodes of all k different colors.
+def IsRainbowDominatingSet {α : Type _} (G : SimpleGraph V) (C : Set V) (f : V → α) : Prop :=
+  ∀ v : V, v ∈ C ∨
+    ∃ (N : Set V), -- N is set of neighbors in C
+      (∀ u ∈ N, G.Adj v u ∧ u ∈ C) ∧
+      -- neighbors' colors cover the entire color space
+      (Set.univ : Set α) = f '' N
+
+noncomputable def k_rainbow_domination_number (k : ℕ) (G : SimpleGraph V) : ℕ :=
+  let S : Set ℕ :=
+    {n | ∃ C : Set V, (∃ f : V → (Fin k), IsRainbowDominatingSet G C f) ∧ n = C.ncard}
+  sInf S
+
+noncomputable def two_rainbow_domination_number (G : SimpleGraph V) : ℕ :=
+  k_rainbow_domination_number 2 G
+
+noncomputable def three_rainbow_domination_number (G : SimpleGraph V) : ℕ :=
+  k_rainbow_domination_number 3 G
 
 -- MATRIX INVARIANTS
 /--
