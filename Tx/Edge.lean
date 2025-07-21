bdd56418d2760c3e1adebd01e129f28d77a2010c
William Hu <williamhu@college.harvard.edu>
2025-07-21 16:49:47 -0400

    roman domination

--- Tx/Edge.lean
+++ Tx/Edge.lean
@@ -63,8 +63,8 @@ The ones listed with an x are still in progress.
   - three_rainbow_domination_number()
   - two_rainbow_domination_number()
 - Roman Domination and Friends
-  x roman_domination_number()
-  x double_roman_domination_number()
+  - roman_domination_number()
+  - double_roman_domination_number()
 - Matrix Invariants and Friends
   - adjacency_eigenvalues()
   - adjacency_matrix()
@@ -268,11 +268,10 @@ noncomputable def well_splitting_number (G : SimpleGraph V) :=
 
 -- DEGREE SEQUENCE BASED
 
-noncomputable def annihilation_number (G : SimpleGraph V) (k : ℕ) :=
+noncomputable def annihilation_number (G : SimpleGraph V) :=
   have degree_list := degree_sequence G
   have m := G.edgeFinset.card
   let sorted_degrees := degree_list.insertionSort (· ≤ ·)
-  -- one way to express \sum_{i = 0}^{t}
   let all_possible_t := {t | (∑ i ∈ Finset.range (t), sorted_degrees[i]!) ≥ m}
   sSup all_possible_t
 
@@ -298,6 +297,36 @@ noncomputable def two_rainbow_domination_number (G : SimpleGraph V) : ℕ :=
 
 noncomputable def three_rainbow_domination_number (G : SimpleGraph V) : ℕ :=
   k_rainbow_domination_number 3 G
+
+-- ROMAN DOMINATION
+
+-- A Roman dominating function on a graph G=(V,E) is a function satisfying the
+-- condition that every vertex u for which f(u)=0 is adjacent to at least one
+-- vertex v for which f(v)=2.
+-- https://www.sciencedirect.com/science/article/pii/S0012365X03004473
+def IsRomanDominatingFunction (G : SimpleGraph V) (f : V → Fin 3) : Prop :=
+  ∀ v : V, f v = 0 → ∃ w, G.Adj v w ∧ f w = 2
+
+noncomputable def roman_domination_number (G : SimpleGraph V) :=
+  let S : Set ℕ :=
+    {n | ∃ f : V → Fin 3, (IsRomanDominatingFunction G f) ∧ n = (∑ v, f v)}
+  sInf S
+
+-- double roman domination; https://www.sciencedirect.com/science/article/pii/S0166218X1630155X
+/--
+f(v) = 0 -> 2 neighbors with 2 or 1 neighbor with 3
+f(v) = 1 -> at least one neighbor geq 2.
+-/
+def IsDoubleRomanDominatingFunction (G : SimpleGraph V) (f : V → Fin 4) : Prop :=
+  ∀ v : V, (f v = 1 → ∃ w, G.Adj v w ∧ f w ≥ 2)
+    ∧ (f v = 0 →
+        (∃ w, G.Adj v w ∧ f w = 3) ∨
+        (∃ w1 w2 : V, w1 ≠ w2 ∧ G.Adj v w1 ∧ G.Adj v w2 ∧ f w1 = 2 ∧ f w2 = 2))
+
+noncomputable def double_roman_domination_number (G : SimpleGraph V) :=
+  let S : Set ℕ :=
+    {n | ∃ f : V → Fin 4, (IsDoubleRomanDominatingFunction G f) ∧ n = (∑ v, f v)}
+  sInf S
 
 -- MATRIX INVARIANTS
 /--
