86d4f7e412d2bdb5e6bda9153817b0914aa64233
William Hu <williamhu@college.harvard.edu>
2025-07-23 19:53:08 -0400

    havel hakimi progress

--- havel_hakimi.lean
+++ havel_hakimi.lean
@@ -44,8 +44,51 @@ def test_function (x : List Nat) : List Nat :=
 -- #eval havel_hakimi_step [6, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1]
 -- #eval havel_hakimi_step [2, 2, 2, 2, 1, 1, 2, 2, 1, 1]
 -- #eval havel_hakimi_step [1, 1, 2, 2, 2, 1, 1, 1, 1]
-
 -- #eval havel_hakimi_full [3, 3, 3, 3]
+
+def havel_hakimi_aux (sorted : List Nat) : List Nat :=
+  match sorted with
+  | [] => []
+  | d :: rest =>
+    if d == 0 then
+      sorted
+    else
+      let split := rest.splitAt d
+      let takeD := split.1
+      let dropD := split.2
+      if takeD.any (· == 0) then
+        sorted
+      else
+        let newRest := (takeD.map (· - 1)) ++ dropD
+        havel_hakimi_aux newRest
+  termination_by sorted.length
+  decreasing_by
+    simp
+    have h₁ : min d rest.length ≤ d := Nat.min_le_left d rest.length
+    have h₂ : min d rest.length + (rest.length - d) ≤ d + (rest.length - d) :=
+      Nat.add_le_add_right h₁ (rest.length - d)
+    -- casework on whether d ≤ rest.length
+    by_cases h_d_le : d ≤ rest.length
+    · have h₃ : d + (rest.length - d) = rest.length := Nat.add_sub_cancel' h_d_le
+      rw [h₃] at h₂
+      exact Nat.lt_of_le_of_lt h₂ (Nat.lt_succ_self rest.length)
+    · have hr0 : rest.length - d = 0 := by
+        rw [Nat.sub_eq_zero_of_le]
+        omega
+      rw [hr0]
+      simp
+    -- have h_split : takeD ++ dropD = rest := by
+    --   unfold takeD
+    --   unfold dropD
+    --   unfold split
+    --   simp
+    -- simp
+    -- have h_len : takeD.length + dropD.length = rest.length := by
+    --   rw [←List.length_append]
+    --   rw [h_split]
+    -- rw [h_len]
+    -- omega
+
 
 def havel_hakimi_full2 (ds : List Nat) : List Nat :=
   let sorted := ds.insertionSort (· ≥ ·)
@@ -62,7 +105,8 @@ def havel_hakimi_full2 (ds : List Nat) : List Nat :=
         let newRest := (takeD.map (· - 1)) ++ dropD
         havel_hakimi_full2 newRest
   termination_by ds.length
-  decreasing_by sorry
+  decreasing_by
+    sorry
   -- decreasing_by
   --   simp
   --   let sorted := ds.insertionSort (· ≥ ·)
