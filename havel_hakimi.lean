4edc78cf0baaf9e7e7652d812ca422e88659f403
William Hu <williamhu@college.harvard.edu>
2025-08-05 22:11:55 -0400

    progress on havel hakimi

--- havel_hakimi.lean
+++ havel_hakimi.lean
@@ -2,9 +2,7 @@ import Mathlib.Data.List.Sort
 import Init.Data.List.Basic
 
 /--
-This currently does one step of the havel_hakimi algorithm.
-A fully recursive version would have to come with a proof of termination,
-which I attempted to complete below in havel_hakimi_full.
+One step of the havel_hakimi algorithm.
 -/
 def havel_hakimi_step (ds : List Nat) : List Nat :=
   let ds := ds.insertionSort (· ≥ ·)
@@ -28,26 +26,14 @@ def havel_hakimi_step (ds : List Nat) : List Nat :=
         let newRest := (takeD.map (· - 1)) ++ dropD
         newRest
 
--- Learning how termination_by works
-def sumList (lst : List Nat) : Nat :=
-  match lst with
-  | []      => 0
-  | x :: xs => x + sumList xs
-termination_by lst
+#eval havel_hakimi_step [1, 1, 1, 1, 1, 1, 1, 1]
 
-def test_function (x : List Nat) : List Nat :=
-  match x with
-  | [] => [12]
-  | a :: _ => a :: [12]
-
--- example from wikipedia
--- #eval havel_hakimi_step [6, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1]
--- #eval havel_hakimi_step [2, 2, 2, 2, 1, 1, 2, 2, 1, 1]
--- #eval havel_hakimi_step [1, 1, 2, 2, 2, 1, 1, 1, 1]
--- #eval havel_hakimi_full [3, 3, 3, 3]
-
-def havel_hakimi_aux (sorted : List Nat) : List Nat :=
-  match sorted with
+/--
+The full Havel-Hakimi algorithm
+-/
+def havel_hakimi_full (ds : List Nat) : List Nat :=
+  let sorted := ds.insertionSort (· ≥ ·)
+  match hdr : sorted with
   | [] => []
   | d :: rest =>
     if d == 0 then
@@ -60,75 +46,40 @@ def havel_hakimi_aux (sorted : List Nat) : List Nat :=
         sorted
       else
         let newRest := (takeD.map (· - 1)) ++ dropD
-        havel_hakimi_aux newRest
-  termination_by sorted.length
+        havel_hakimi_full newRest
+  termination_by ds.length
   decreasing_by
     simp
+    have hsd : sorted.length = ds.length := by
+      unfold sorted
+      apply List.length_insertionSort (· ≥ ·)
+    have hr : rest.length < sorted.length := by
+      rw [hdr]
+      rw [List.length_cons]
+      omega
     have h₁ : min d rest.length ≤ d := Nat.min_le_left d rest.length
+    have h₁' : min d rest.length ≤ rest.length := Nat.min_le_right d rest.length
     have h₂ : min d rest.length + (rest.length - d) ≤ d + (rest.length - d) :=
       Nat.add_le_add_right h₁ (rest.length - d)
     -- casework on whether d ≤ rest.length
     by_cases h_d_le : d ≤ rest.length
     · have h₃ : d + (rest.length - d) = rest.length := Nat.add_sub_cancel' h_d_le
       rw [h₃] at h₂
-      exact Nat.lt_of_le_of_lt h₂ (Nat.lt_succ_self rest.length)
+      have h₄ : min d rest.length + (rest.length - d) ≤ rest.length := h₂
+      rw [←hsd]
+      apply lt_of_le_of_lt h₄ hr
     · have hr0 : rest.length - d = 0 := by
         rw [Nat.sub_eq_zero_of_le]
         omega
+      rw [←hsd]
+      rw [hr0] at h₂
       rw [hr0]
-      simp
-    -- have h_split : takeD ++ dropD = rest := by
-    --   unfold takeD
-    --   unfold dropD
-    --   unfold split
-    --   simp
-    -- simp
-    -- have h_len : takeD.length + dropD.length = rest.length := by
-    --   rw [←List.length_append]
-    --   rw [h_split]
-    -- rw [h_len]
-    -- omega
+      rw [Nat.add_zero]
+      apply lt_of_le_of_lt h₁'
+      apply hr
 
-
-def havel_hakimi_full2 (ds : List Nat) : List Nat :=
-  let sorted := ds.insertionSort (· ≥ ·)
-  match sorted with
-  | [] => []
-  | d :: rest =>
-    if d == 0 then
-      sorted
-    else
-      let (takeD, dropD) := rest.splitAt d
-      if takeD.any (· == 0) then
-        sorted
-      else
-        let newRest := (takeD.map (· - 1)) ++ dropD
-        havel_hakimi_full2 newRest
-  termination_by ds.length
-  decreasing_by
-    sorry
-  -- decreasing_by
-  --   simp
-  --   let sorted := ds.insertionSort (· ≥ ·)
-  --   match sorted with
-  --   | [] => trivial
-  --   | d :: rest =>
-  --     split_ifs with h1 h2
-  --     let sorted := ds.insertionSort (· ≥ ·)
-
-def havel_hakimi_full (ds : List Nat) : List Nat :=
-  match ds.insertionSort (· ≥ ·) with
-  | [] => []
-  | d :: rest =>
-    if d == 0 then
-      d :: rest
-    else
-      let (takeD, dropD) := rest.splitAt d
-      if takeD.any (· == 0) then
-        d :: rest
-      else
-        let newRest := (takeD.map (· - 1)) ++ dropD
-        havel_hakimi_full newRest
-termination_by ds.length
-decreasing_by
-sorry
+-- example from wikipedia
+-- #eval havel_hakimi_step [6, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1]
+-- #eval havel_hakimi_step [2, 2, 2, 2, 1, 1, 2, 2, 1, 1]
+-- #eval havel_hakimi_step [1, 1, 2, 2, 2, 1, 1, 1, 1]
+-- #eval havel_hakimi_full [3, 3, 3, 3]
