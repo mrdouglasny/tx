2e9c4b436d98e78e206e8b35e98b1c64572ef707
William Hu <williamhu@college.harvard.edu>
2025-08-16 20:24:09 -0400

    Add a single file that imports all others.

---
+++ TxGraffitiBench/Imports.lean
@@ -0,0 +1,14 @@
+import TxGraffitiBench.Invariants.Basic
+import TxGraffitiBench.Invariants.DegreeSequence
+import TxGraffitiBench.Invariants.HavelHakimi
+import TxGraffitiBench.Invariants.Matrix
+import TxGraffitiBench.Invariants.Sugar
+import TxGraffitiBench.Invariants.Domination.Basic
+import TxGraffitiBench.Invariants.Domination.Power
+import TxGraffitiBench.Invariants.Domination.Rainbow
+import TxGraffitiBench.Invariants.Domination.Roman
+
+/-
+This file is used to import everything,
+like https://github.com/openai/miniF2F/tree/main/lean/src
+-/
