2cdef7de641c71119c954fdc7ff8403adaed66ae
William Hu <williamhu@college.harvard.edu>
2025-09-01 15:14:02 -0400

    Add sugar for graph size (number of edges)

--- TxGraffitiBench/Invariants/Sugar.lean
+++ TxGraffitiBench/Invariants/Sugar.lean
@@ -6,6 +6,8 @@ universe u
 
 variable {V : Type u} [Fintype V] [DecidableEq V]
 
+open Classical
+
 /--
 SHIM DEFINITIONS FOR CONJECTURE USAGE
 The output of TxGraffiti2 uses slighly different names which we define as syntactic sugar here.
@@ -14,6 +16,7 @@ This allows the output to be effectively directly exportable into Lean.
 alias max_degree := maximum_degree
 alias min_degree := minimum_degree
 def order (_ : SimpleGraph V) := Fintype.card V
+noncomputable def size (G : SimpleGraph V) := G.edgeFinset.card
 def connected (G : SimpleGraph V) := G.Connected
 def bipartite (G : SimpleGraph V) := G.IsBipartite
 
