b34f4d18dc0bb53095dccac776c7b76dca43e303
William Hu <williamhu@college.harvard.edu>
2025-09-01 13:48:56 -0400

    Remove Hammer dependency

--- lakefile.lean
+++ lakefile.lean
@@ -7,9 +7,6 @@ package «TxGraffitiBench» where
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
   ]
   -- add any additional package configuration options here
-require Hammer from git
-  "https://github.com/JOSHCLUNE/LeanHammer.git"@"v4.21.0"
-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"v4.21.0"
 
